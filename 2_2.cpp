99b32ba529bd963c57bf3c4c54e3c185bf469013
dev <46674147+dev7060@users.noreply.github.com>
2021-05-03 04:44:56 +0530

    Create 2_2.cpp

---
+++ 2_2.cpp
@@ -0,0 +1,91 @@
+/*Enter your code here. Read input from STDIN. Print your output to STDOUT*/
+#include <iostream>
+using namespace std;
+int is_prime(long long int num){
+    if(num>=10){
+        if(num%10==0||num%10==2||num%10==4||num%10==6||num%10==8){
+            return 1;
+        }
+    }
+    int flag=0; //1 for non prime, 2 for prime
+     for(long long int i=2; i<=(num/2); i++){
+        if(num%i==0){
+            flag=1;
+            break;
+        }
+    }
+    if(flag==1){
+        return 1; //ie not prime
+    }else if(flag == 0){
+        return 5; //prime
+    }
+    
+}
+int no_prime_check(long long int l, long long int r){
+    //long long int current = l;
+    int flag=0; //1-list has atleast one prime no, 2-all list is non prime
+    int count=0;
+    while(l<=r){
+        if(is_prime(l)==5){
+            flag=1;
+            count++;
+        }
+        l++;
+    }
+   // cout<<" count:"<<count<<" ";
+    if(flag==1){
+        //list has atleast 1 prime
+        if(count==1){
+            return 3;
+        }else{
+            return 1;
+        }
+        
+    }else if(flag==0){
+        //all list non prime
+        return 5;
+    }
+    //return 5;
+   
+}
+
+int main()
+{
+	long long int n;
+	long long int l, r;
+	cin>>n;
+	while(n--){
+	    cin>>l>>r;
+	    /*
+	    if(l==r){
+	        cout<<"0\n";
+	    }else
+	    */
+	    if(no_prime_check(l, r)==5){
+	        cout<<"-1\n";
+	    }else if(no_prime_check(l, r)==3){
+	        cout<<"0\n";
+	    }else{
+	        long long int new_l, new_r;
+	        for(long long int temp1=l; temp1<=r; temp1++){
+	            if(is_prime(temp1)==5){
+	                new_l=temp1;
+	                break;
+	            }
+	        }
+	        for(long long int temp1=r; temp1>=l; temp1--){
+	            if(is_prime(temp1)==5){
+	                new_r=temp1;
+	                break;
+	            }
+	        }
+	        //cout<< new_l <<" "<<new_r<<" ";
+	        
+	        if(new_r==new_l){
+	            cout<<new_l<<"\n";
+	        }else{
+	            cout<<new_r-new_l<<"\n";
+	        }
+	    }
+	}
+}
